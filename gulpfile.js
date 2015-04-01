6314846d0f6f9a7fcf61f2f984dd8587d79165b5
Alex Mueller <ajmueller6@gmail.com>
2015-03-31 18:22:00 -0700

    Moves Gulp tasks into separate tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,22 @@ var sass = require('gulp-sass');
 var sassdoc = require('sassdoc');
 var styledown = require('gulp-styledown');
 
-gulp.task('default', function () {
-	gulp.src('./src/css/**/*.scss')
+gulp.task('sassdoc', function() {
+	return gulp.src('./src/css/**/*.scss')
 		.pipe(sassdoc({
 			dest: './documentation/api'
 		}))
 		.pipe(sass())
 		.pipe(gulp.dest('./dist/css'));
+});
 
-	gulp.src('./src/css/**/*.scss')
+gulp.task('styledown', function() {
+	return gulp.src('./src/css/**/*.scss')
 		.pipe(styledown({
 			config: 'config.md',
 			filename: 'index.html'
 		}))
 		.pipe(gulp.dest('./documentation/style-guide/'));
 });
+
+gulp.task('default', ['sassdoc', 'styledown']);
